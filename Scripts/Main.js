9aa9820c53c5fbb75597cec30bde3f3012e3d633
Fancryer <vadimir.kots@mail.ru>
2022-06-24 10:04:51 +0400

    Hello

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -36,10 +36,12 @@ class ManufacturersContainer
             let IteratedManufacturer=document.querySelector(`div[name="ManufacturerDiv${i}"]`);
             if(IteratedManufacturer.children[1].checked)Manufacturers.push(IteratedManufacturer.children[0].innerHTML);
         }
-        colog(Manufacturers);
+        //colog(Manufacturers);
         return Manufacturers;
-    };
+    }
 }
+
+const MainManufacturersContainer=new ManufacturersContainer;
 
 class DiagnosesContainer
 {
@@ -78,50 +80,54 @@ class DiagnosesContainer
             let IteratedDiagnosis=document.querySelector(`div[name="DiagnosisDiv${i}"]`);
             if(IteratedDiagnosis.children[1].checked)Diagnoses.push(IteratedDiagnosis.children[0].innerHTML);
         }
-        colog(Diagnoses);
+        //colog(Diagnoses);
         return Diagnoses;
-    };
+    }
 }
 
-const MainManufacturersContainer=new ManufacturersContainer;
 const MainDiagnosesContainer=new DiagnosesContainer;
 
-function AddToothpasteBox
-(
-    ToothpasteName="Toothpaste name",
-    Diagnoses=["Профилактика"],
-    HasFluorine=false,
-    ImageSRC="../Resources/Images/ColgateBlue.jpg",
-    Manufacturer="Colgate, США",
-    ConsumerAge="Для всех возрастов",
-    NeedToBright=false
-)
+class StorageOverlord
 {
-    let PastContainer=document.querySelector("main");
-    let DiagnArr=Diagnoses.join(", ");
-    colog(Manufacturer);
-    PastContainer.insertAdjacentHTML
-    (
-        "beforeend",
-        `<div class="ToothpasteBox">
-            <div class="Name">${ToothpasteName}</div>
-            <div class="ToothpasteRow">
-                <div style="position:relative">
-                    <img
-                        class="PastImage"
-                        src=${ImageSRC}
-                    ></img>
-                    <div style="position:absolute;bottom:5%;left:5%;color:${NeedToBright?"white":"black"}">${ConsumerAge}</div>
-                </div>
-                <div class="ToothpasteDetails">
-                    <div class="Diagnosis">Диагноз${Diagnoses.length-1?"ы":""}: ${DiagnArr}</div>
-                    <div class="Manufacturer">Производитель: ${Manufacturer}</div>
-                    <div class="Fluorine">${HasFluorine?"Со фтором":"Без фтора"}</div>
-                </div>
-            </div>
-        </div>`
-    );
+    constructor()
+    {
+        this.BackupStorage=
+        {
+            FluorineChecked:
+                "Indif",
+            SelectedAge:
+                "Для всех возрастов",
+            SelectedManufacturers:
+                MainManufacturersContainer.GetAllManufacturers().map(element=>[element,true]),
+            SelectedDiagnoses:
+                MainDiagnosesContainer.GetAllDiagnoses().map(element=>[element,true])
+        }
+        this.Storage=
+        {
+            FluorineChecked:
+                localStorage.getItem("FluorineChecked")?localStorage.getItem("FluorineChecked"):"Indif",
+            SelectedAge:
+                localStorage.getItem("SelectedAge")?localStorage.getItem("SelectedAge"):"Для всех возрастов",
+            SelectedManufacturers:
+                localStorage.getItem("SelectedManufacturers")?JSON.parse(localStorage.getItem("SelectedManufacturers")):
+                MainManufacturersContainer.GetAllManufacturers().map(element=>[element,true]),
+            SelectedDiagnoses:
+                localStorage.getItem("SelectedDiagnoses")?JSON.parse(localStorage.getItem("SelectedDiagnoses")):
+                MainDiagnosesContainer.GetAllDiagnoses().map(element=>[element,true]),
+            Tastes:[]
+        };
+    }
+    //RestoreFromBackup(){this.Storage=JSON.parse(JSON.stringify(this.BackupStorage));localStorage.clear();}
 }
+
+const MainStorageOverlord=new StorageOverlord;
+//MainStorageOverlord.LoadFromLocalStorage();
+
+colog(MainStorageOverlord);
+
+
+
+
 
 class Toothpaste
 {
@@ -197,177 +203,222 @@ const Pastes=
     )
 ];
 
-function InitPage()
+class PageFiller
 {
-    let InitDiagnosesFieldset=()=>
+    AddToothpasteBox
+    (
+        ToothpasteName="Toothpaste name",
+        Diagnoses=["Профилактика"],
+        HasFluorine=false,
+        ImageSRC="../Resources/Images/ColgateBlue.jpg",
+        Manufacturer="Colgate, США",
+        ConsumerAge="Для всех возрастов",
+        NeedToBright=false
+    )
     {
-        let DiagnosesFieldset=document.querySelector("fieldset#DiagnosesFieldset");
-        DiagnosesFieldset.insertAdjacentHTML
+        let PastContainer=document.querySelector("main");
+        let DiagnArr=Diagnoses.join(", ");
+        //colog(Manufacturer);
+        PastContainer.insertAdjacentHTML
         (
             "beforeend",
-            `<legend>Диагноз</legend>\n`
+            `<div class="ToothpasteBox">
+                <div class="Name">${ToothpasteName}</div>
+                <div class="ToothpasteRow">
+                    <div style="position:relative">
+                        <img
+                            class="PastImage"
+                            src=${ImageSRC}
+                        ></img>
+                        <div style="position:absolute;bottom:5%;left:5%;color:${NeedToBright?"white":"black"}">${ConsumerAge}</div>
+                    </div>
+                    <div class="ToothpasteDetails">
+                        <div class="Diagnosis">Диагноз${Diagnoses.length-1?"ы":""}: ${DiagnArr}</div>
+                        <div class="Manufacturer">Производитель: ${Manufacturer}</div>
+                        <div class="Fluorine">${HasFluorine?"Со фтором":"Без фтора"}</div>
+                    </div>
+                </div>
+            </div>`
         );
-        for(let i=0;i<MainDiagnosesContainer.GetDiagnosesCount();++i)
-        {
-            DiagnosesFieldset.insertAdjacentHTML
-            (
-                "beforeend",
-                `<div name="DiagnosisDiv${i}" style="display:flex;min-height:25px;">
-                    <label>${MainDiagnosesContainer.GetDiagnosisByIndex(i)}</label>
-                    <input name="Diagnosis" form="FilterForm" type="checkbox" id="Diagn${i}" checked>
-                </div>\n`
-            )
-        }
-    };
-    InitDiagnosesFieldset();
-    let InitManufacturersFieldset=()=>
+    }
+
+    FilterBoxes()
     {
-        let Manufacturers=MainManufacturersContainer.GetAllManufacturers();
-        let ManufacturersFieldSet=document.querySelector("fieldset#ManufacturersFieldset");
-        for(let i=0;i<Manufacturers.length;++i)
-        {
-            ManufacturersFieldSet.insertAdjacentHTML
-            (
-                "beforeend",
-                `<div name="ManufacturerDiv${i}" style="display:flex;min-height:20px;padding-bottom:10px">
-                    <label>${Manufacturers[i]}</label>
-                    <input name="Manufacturer" form="FilterForm" type="checkbox" id="Manufacturer${i}" checked>
-                </div>\n`
-            );
-        };
-    };
-    InitManufacturersFieldset();
-    for(let i=0;i<Pastes.length;++i)
-    {
-        AddToothpasteBox
+        let ToothpasteBox=document.querySelectorAll("div.ToothpasteBox");
+
+        //let FluorineChoice=document.querySelector("input[type=radio]:checked").id;
+        localStorage.setItem
         (
-            Pastes[i].Name,
-            Pastes[i].Diagnoses,
-            Pastes[i].HasFluorine,
-            Pastes[i].ImageSRC,
-            Pastes[i].Manufacturer,
-            Pastes[i].ConsumerAge,
-            ["Doramad Xtasy"].includes(Pastes[i].Name)
+            "FluorineChecked",
+            MainStorageOverlord.FluorineChecked=document.querySelector("input[type=radio]:checked").id.replace("Fluorine","")
+        );//Set localStorage variable and MainStorageOverlord variable
+        let FluorineChecked=MainStorageOverlord.FluorineChecked=="Indif"?null:MainStorageOverlord.FluorineChecked=="True"?true:false;//Set local variable for filtering
+        //colog(MainStorageOverlord.FluorineChecked);
+
+        
+        let SelectedAge=document.querySelector("input[list=AgeList]").value;//Set local variable for filtering
+        localStorage.setItem("SelectedAge",MainStorageOverlord.Storage.SelectedAge=SelectedAge);//Set localStorage variable
+
+        let SelectedManufacturers=MainManufacturersContainer.GetSelectedManufacturers();
+        MainStorageOverlord.Storage.SelectedManufacturers.forEach((element,i)=>{element[1]=SelectedManufacturers.includes(element[0]);});
+        localStorage.setItem("SelectedManufacturers",JSON.stringify(MainStorageOverlord.Storage.SelectedManufacturers));
+
+        let SelectedDiagnoses=MainDiagnosesContainer.GetSelectedDiagnoses();
+        MainStorageOverlord.Storage.SelectedDiagnoses.forEach((element,i)=>{element[1]=SelectedDiagnoses.includes(element[0]);});
+        localStorage.setItem("SelectedDiagnoses",JSON.stringify(MainStorageOverlord.Storage.SelectedDiagnoses));
+
+        let Counter=0;
+    
+        ToothpasteBox.forEach
+        (
+            (element,i)=>
+            {
+                colog(`Index: ${i} (${element.children[0].innerHTML})`);
+                
+                //Filter by fluorine
+                let DeservedVisibility=FluorineChecked==Pastes[i].HasFluorine||FluorineChecked==null;
+                console.log
+                (
+                    `%c${DeservedVisibility?"\tFluorine matches!":"\tFluorine doesn't match..."}`,
+                    `background: #222; color: ${DeservedVisibility?"#bada55":"#ffab55"}`
+                );
+
+                //Filter by age
+                DeservedVisibility&&=[Pastes[i].ConsumerAge,"","Для всех возрастов"].includes(SelectedAge);
+                console.log
+                (
+                    `%c${DeservedVisibility?"\t\tAge matches!":"\t\tAge doesn't match or the predicate above is false..."}`,
+                    `background: #222; color: ${DeservedVisibility?"#bada55":"#ffab55"}`
+                );
+
+                //Filter by manufacturer
+                DeservedVisibility&&=SelectedManufacturers?SelectedManufacturers.includes(Pastes[i].Manufacturer):false;
+                console.log
+                (
+                    `%c${DeservedVisibility?"\t\t\tManufacturer matches!":"\t\t\tManufacturer doesn't match or the predicate above is false..."}`,
+                    `background: #222; color: ${DeservedVisibility?"#bada55":"#ffab55"}`
+                );
+
+                //Filter by diagnosis
+                DeservedVisibility&&=SelectedDiagnoses?Pastes[i].Diagnoses.some(item=>SelectedDiagnoses.includes(item)):false;
+                console.log
+                (
+                    `%c${DeservedVisibility?"\t\t\t\tDiagnosis matches!":"\t\t\t\tDiagnosis doesn't match or the predicate above is false..."}`,
+                    `background: #222; color: ${DeservedVisibility?"#bada55":"#ffab55"}`
+                );
+                
+                element.style.display=DeservedVisibility?"flex":"none";
+                if(DeservedVisibility)++Counter;
+            }
+        )
+        console.log
+        (
+            `%cAmount of boxes that was showed: ${Counter}`,
+            `background: #111; color: ${Counter?"#bada55":"#ddaabb"}`
         );
-        colog(Pastes[i]);
+        colog(localStorage);
+        return false;
     }
-    FilterBoxes();
-}
 
-InitPage();
-
-function FilterBoxes()
-{
-    let ToothpasteBox=document.querySelectorAll("div.ToothpasteBox");
-    let FluorineChecked=document.querySelector("input[type=radio]:checked").id;
-    FluorineChecked=FluorineChecked=="FluorineTrue"?true:FluorineChecked=="FluorineFalse"?false:null;
-    let SelectedAge=document.querySelector("input[list=AgeList]").value;
-    let SelectedManufacturers=MainManufacturersContainer.GetSelectedManufacturers();
-    let SelectedDiagnoses=MainDiagnosesContainer.GetSelectedDiagnoses();
+    /*
+    RefreshBoxes()
+    {
+        MainStorageOverlord.RestoreFromBackup();
+        this.FilterBoxes();
+    }
+    */
 
 
-    ToothpasteBox.forEach
-    (
-        (element,i)=>
+    InitPage()
+    {
+        let InitFluorineRadio=()=>
         {
-            colog(`Index: ${i} (${element.children[0].innerHTML})`);
-
-            //Filter by fluorine
-            let DeservedVisibility=FluorineChecked==Pastes[i].HasFluorine||FluorineChecked==null;
-            colog(`${DeservedVisibility?"\tFluorine matches!":"\tFluorine doesn't match..."}`);
-            
-            //Filter by age
-            DeservedVisibility&&=[Pastes[i].ConsumerAge,"","Для всех возрастов"].includes(SelectedAge);
-            colog(`${DeservedVisibility?"\t\tAge matches!":"\t\tAge doesn't match..."}`);
-
-            //Filter by manufacturer
-            DeservedVisibility&&=SelectedManufacturers?SelectedManufacturers.includes(Pastes[i].Manufacturer):false;
-            colog(`${DeservedVisibility?"\t\t\tManufacturer matches!":"\t\t\tManufacturer doesn't match..."}`);
-            
-            //Filter by diagnosis
-            DeservedVisibility&&=SelectedDiagnoses?Pastes[i].Diagnoses.some(item=>SelectedDiagnoses.includes(item)):false;
-            colog(`${DeservedVisibility?"\t\t\t\tDiagnosis matches!":"\t\t\t\tDiagnosis doesn't match..."}`);
-
-            element.style.display=DeservedVisibility?"flex":"none";
+            let FluorineChecked=MainStorageOverlord.Storage.FluorineChecked;
+            document.querySelector(`input[type=radio]#Fluorine${FluorineChecked}`).checked=true;
         }
-    )
-}
+        InitFluorineRadio();
 
-
-
-
-
-/*
-let ToothpasteBox=document.querySelectorAll("div.ToothpasteBox");
-function FilterFluorine()
-{
-    let FluorineChecked=document.querySelector("input[type=radio]:checked").id;
-    if(FluorineChecked=="FluorineTrue")FluorineChecked=true;
-    else if(FluorineChecked=="FluorineFalse")FluorineChecked=false;
-    else FluorineChecked=null;
-    colog(FluorineChecked);
-    colog(ToothpasteBox.length);
-    ToothpasteBox.forEach((element,i)=>{element.style.display=FluorineChecked==null||Pastes[i].HasFluorine==FluorineChecked?"flex":"none";});
-    colog(ToothpasteBox.length);
-}
-FilterFluorine();
-function FilterManufacturers()
-{
-    let Manufacturers=MainManufacturersContainer.GetAllManufacturers();
-    let ManufacturersCheckboxes=[];
-    colog(Manufacturers);
-    for(let i=0;i<Manufacturers.length;++i)
-    {
-        let checkbox=document.querySelector(`#Manufacturer${i}`).checked;
-        if(checkbox)ManufacturersCheckboxes.push(Manufacturers[i]);
-    }
-    colog(ManufacturersCheckboxes);
-    for(let i=0;i<ToothpasteBox.length;++i)
-    {
-        //div.ToothpasteRow->div.ToothpasteDetails->div.Manufacturer
-        let CurrentManifacturer=ToothpasteBox[i].children.item(1).children.item(1).children.item(1);
-        colog(CurrentManifacturer);
-        let Found=false;
-        for(let m=0;m<ManufacturersCheckboxes.length;++m)
+        let InitAgeList=()=>
         {
-            if(ManufacturersCheckboxes[m]==CurrentManifacturer.innerHTML.split(": ")[1])
+            let SelectedAge=MainStorageOverlord.Storage.SelectedAge;
+            document.querySelector("input[list=AgeList]").value=SelectedAge;
+        }
+        InitAgeList();
+
+        let InitDiagnosesFieldset=()=>
+        {
+            let DiagnosesFieldset=document.querySelector("fieldset#DiagnosesFieldset");
+            DiagnosesFieldset.insertAdjacentHTML("beforeend","<legend>Диагноз</legend>\n");
+            for(let i=0;i<MainDiagnosesContainer.GetDiagnosesCount();++i)
             {
-                CurrentManifacturer.parentElement.parentElement.parentElement.style.display="flex";
-                colog("Yes");Found=true;break;
+                DiagnosesFieldset.insertAdjacentHTML
+                (
+                    "beforeend",
+                    `<div name="DiagnosisDiv${i}" style="display:flex;min-height:25px;">
+                        <label>${MainDiagnosesContainer.GetDiagnosisByIndex(i)}</label>
+                        <input name="Diagnosis" type="checkbox" id="Diagn${i}"${MainStorageOverlord.Storage.SelectedDiagnoses[i][1]?" checked":""}>
+                    </div>\n`
+                )
             }
-        }
-        if(!Found)CurrentManifacturer.parentElement.parentElement.parentElement.style.display="none";
-    }
-}
-FilterManufacturers();
-function FilterDiagnoses()
-{
-    let Diagnoses=DiagnosesArr;
-    let DiagnosesCheckboxes=[];
-    colog(Diagnoses);
-    for(let i=0;i<Diagnoses.length;++i)
-    {
-        let diagn=document.querySelector(`#Diagn${i}`);
-        let checkbox=diagn.checked;
-        if(checkbox&&diagn.style.display!="none")DiagnosesCheckboxes.push(Diagnoses[i]);
-    }
-    colog(DiagnosesCheckboxes);
-    for(let i=0;i<ToothpasteBox.length;++i)
-    {
-        //div.ToothpasteRow->div.ToothpasteDetails->div.Manufacturer
-        let CurrentDiagnose=ToothpasteBox[i].children.item(1).children.item(1).children.item(1);
-        colog(CurrentDiagnose);
-        let Found=false;
-        for(let m=0;m<DiagnosesCheckboxes.length;++m)
+        };
+        InitDiagnosesFieldset();
+
+        let InitManufacturersFieldset=()=>
         {
-            if(DiagnosesCheckboxes[m]==CurrentDiagnose.innerHTML.split(": ")[1])
+            let Manufacturers=MainManufacturersContainer.GetAllManufacturers();
+            let ManufacturersFieldSet=document.querySelector("fieldset#ManufacturersFieldset");
+            ManufacturersFieldSet.insertAdjacentHTML("beforeend","<legend>Производитель</legend>\n");
+            for(let i=0;i<Manufacturers.length;++i)
             {
-                CurrentDiagnose.parentElement.parentElement.parentElement.style.display="flex";
-                colog("Yes");Found=true;break;
-            }
+                ManufacturersFieldSet.insertAdjacentHTML
+                (
+                    "beforeend",
+                    `<div name="ManufacturerDiv${i}" style="display:flex;min-height:20px;padding-bottom:10px">
+                        <label>${Manufacturers[i]}</label>
+                        <input name="Manufacturer" type="checkbox" id="Manufacturer${i}"${MainStorageOverlord.Storage.SelectedManufacturers[i][1]?" checked":""}>
+                    </div>\n`
+                );
+            };
+        };
+        InitManufacturersFieldset();
+        
+        let InitTastesFieldset=()=>
+        {
+            let Manufacturers=MainManufacturersContainer.GetAllManufacturers();
+            let TastesFieldSet=document.querySelector("fieldset#TastesFieldset");
+            TastesFieldSet.insertAdjacentHTML("beforeend","<legend>Вкус</legend>\n");
+            for(let i=0;i<Manufacturers.length;++i)
+            {
+                TastesFieldSet.insertAdjacentHTML
+                (
+                    "beforeend",
+                    `<div name="TasteDiv${i}" style="display:flex;min-height:20px;padding-bottom:10px">
+                        <label>${Manufacturers[i]}</label>
+                        <input name="Taste" type="checkbox" id="Taste${i}"${MainStorageOverlord.Storage.SelectedManufacturers[i][1]?" checked":""}>
+                    </div>\n`
+                );
+            };
+        };
+        InitTastesFieldset();
+
+
+        for(let i=0;i<Pastes.length;++i)
+        {
+            this.AddToothpasteBox
+            (
+                Pastes[i].Name,
+                Pastes[i].Diagnoses,
+                Pastes[i].HasFluorine,
+                Pastes[i].ImageSRC,
+                Pastes[i].Manufacturer,
+                Pastes[i].ConsumerAge,
+                ["Doramad Xtasy"].includes(Pastes[i].Name)
+            );
+            //colog(Pastes[i]);
         }
-        if(!Found)CurrentDiagnose.parentElement.parentElement.parentElement.style.display="none";
+        this.FilterBoxes();
     }
 }
-FilterDiagnoses();
-*/
+
+let MainPageFiller=new PageFiller;
+MainPageFiller.InitPage();
